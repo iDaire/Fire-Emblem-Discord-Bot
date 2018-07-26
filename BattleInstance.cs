ee1731cff92bdb0c8a089fb4757e4ac704bc2450
Dairy King <iPyrofuze@gmail.com>
2018-07-26 00:53:39 -0400

    Getting The Hello World Function working properly.

--- BattleInstance.cs
+++ BattleInstance.cs
@@ -2,6 +2,7 @@ using System;
 
 class BattleInstance {
     public static void Main (String[] args) {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Hello World!"); // Default hello world message.
+        Console.ReadLine(); // Holding the console window open.
     }
 }
